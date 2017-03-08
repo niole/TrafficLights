2496c03001ec35c7466dc81b9aa0eab479aafcaa
niole <niolenelson@gmail.com>
2017-03-08 13:39:44 -0800

    traffic light goes red, yellow, green, yellow, red

--- trafficLights.hs
+++ trafficLights.hs
@@ -9,11 +9,12 @@ trafficLight :: String -> Picture
 trafficLight "red" = singleLight black (-3) & singleLight black 0 & singleLight red 3 & frame
 trafficLight "yellow" = singleLight black (-3) & singleLight yellow 0 & singleLight black 3 & frame
 trafficLight "green" = singleLight green (-3) & singleLight black 0 & singleLight black 3 & frame
+trafficLight _ = singleLight black (-3) & singleLight black 0 & singleLight black 3 & frame
 
 trafficController :: Double -> Picture
 trafficController t
-  | (round(t) `mod` 7) >= 0 && (round(t) `mod` 7) < 3  = trafficLight "red"
-  | (round(t) `mod` 7) <= 6 && (round(t) `mod` 7) >= 4  = trafficLight "green"
+  | (round(t) `mod` 14) < 3 || (round(t) `mod` 14) > 10 = trafficLight "red"
+  | (round(t) `mod` 14) >=5  && (round(t) `mod` 14) <= 8  = trafficLight "green"
   | otherwise                = trafficLight "yellow"
 
 main :: IO ()

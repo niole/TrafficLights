de815579f98ad4d678831bf853213768d9deeae8
niole <niolenelson@gmail.com>
2017-03-08 13:24:25 -0800

    lights change and are in correct position, frame sized correctly

--- trafficLights.hs
+++ trafficLights.hs
@@ -3,16 +3,18 @@ import CodeWorld
 singleLight :: Color -> Double -> Picture
 singleLight color pos = colored color (translated 0 pos (solidCircle 1))
 
-trafficLight :: Color -> Picture
-trafficLight red = singleLight black 0 & singleLight black 3 & singleLight red 6
-trafficLight yellow = singleLight black 0 & singleLight yellow 3 & singleLight black 6
-trafficLight green = singleLight green 0 & singleLight black 3 & singleLight black 6
+frame = rectangle 2.5 9.0
+
+trafficLight :: String -> Picture
+trafficLight "red" = singleLight black (-3) & singleLight black 0 & singleLight red 3 & frame
+trafficLight "yellow" = singleLight black (-3) & singleLight yellow 0 & singleLight black 3 & frame
+trafficLight "green" = singleLight green (-3) & singleLight black 0 & singleLight black 3 & frame
 
 trafficController :: Double -> Picture
 trafficController t
-  | (round(t) `mod` 7) >= 0 && (round(t) `mod` 7) < 3  = trafficLight red
-  | (round(t) `mod` 7) <= 6 && (round(t) `mod` 7) >= 4  = trafficLight green
-  | otherwise                = trafficLight yellow
+  | (round(t) `mod` 7) >= 0 && (round(t) `mod` 7) < 3  = trafficLight "red"
+  | (round(t) `mod` 7) <= 6 && (round(t) `mod` 7) >= 4  = trafficLight "green"
+  | otherwise                = trafficLight "yellow"
 
 main :: IO ()
 main = animationOf trafficController

59b44871befcb281cf916441c0778071ef42543f
niole <niolenelson@gmail.com>
2017-03-08 13:16:58 -0800

    traffic lights change color

--- trafficLights.hs
+++ trafficLights.hs
@@ -1,6 +1,18 @@
 import CodeWorld
 
-ourPicture :: Picture
-ourPicture = blank
+singleLight :: Color -> Double -> Picture
+singleLight color pos = colored color (translated 0 pos (solidCircle 1))
 
-main = drawingOf ourPicture
+trafficLight :: Color -> Picture
+trafficLight red = singleLight black 0 & singleLight black 3 & singleLight red 6
+trafficLight yellow = singleLight black 0 & singleLight yellow 3 & singleLight black 6
+trafficLight green = singleLight green 0 & singleLight black 3 & singleLight black 6
+
+trafficController :: Double -> Picture
+trafficController t
+  | (round(t) `mod` 7) >= 0 && (round(t) `mod` 7) < 3  = trafficLight red
+  | (round(t) `mod` 7) <= 6 && (round(t) `mod` 7) >= 4  = trafficLight green
+  | otherwise                = trafficLight yellow
+
+main :: IO ()
+main = animationOf trafficController
